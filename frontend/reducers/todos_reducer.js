ed3c332c469c5f207a7cc68e80e2a87cdc6f7e23
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-12 23:24:31 -0700

    Add todo reducer

---
+++ frontend/reducers/todos_reducer.js
@@ -0,0 +1,25 @@
+import { RECEIVE_TODOS, RECEIVE_TODO, REMOVE_TODO} from '../actions/todo_actions';
+import merge from 'lodash/merge';
+
+const todosReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let nextState;
+
+  switch(action.type){
+    case RECEIVE_TODOS:
+      nextState = {};
+      action.todos.forEach(todo => nextState[todo.id] = todo);
+      return nextState;
+    case RECEIVE_TODO:
+      const newTodo = {[action.todo.id]: action.todo};
+      return merge({}, state, newTodo);
+    case REMOVE_TODO:
+      nextState = merge({}, state);
+      delete nextState[action.todo.id];
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
