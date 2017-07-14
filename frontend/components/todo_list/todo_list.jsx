ed7f60125dd484d0e6fdde87267ed6b8e295adf3
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-13 21:44:12 -0700

    Fix jQuery requests

--- frontend/components/todo_list/todo_list.jsx
+++ frontend/components/todo_list/todo_list.jsx
@@ -3,21 +3,21 @@ import TodoListItem from './todo_list_item';
 
 class TodoList extends React.Component {
   componentDidMount() {
-    // this.props.requestTodos();
+    this.props.requestTodos();
   }
 
   render() {
     const { todos } = this.props;
     console.log(this.props);
-    // const todoItems = todos.map(todo => (
-    //   <TodoListItem key={ todo.id } todo={ todo } />
-    //   )
-    // );
+    const todoItems = todos.map(todo => (
+      <TodoListItem key={ todo.id } todo={ todo } />
+      )
+    );
 
     return (
       <div>
         <ul className="todo-list">
-          todoItems
+          {todoItems}
         </ul>
       </div>
     );
--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,10 +1,9 @@
-import { RECEIVE_TODOS, RECEIVE_TODO, REMOVE_TODO} from '../actions/todo_actions';
+import { RECEIVE_TODOS, RECEIVE_TODO} from '../actions/todo_actions';
 import merge from 'lodash/merge';
 
 const todosReducer = (state = {}, action) => {
   Object.freeze(state);
   let nextState;
-
   switch(action.type){
     case RECEIVE_TODOS:
       nextState = {};
@@ -13,10 +12,6 @@ const todosReducer = (state = {}, action) => {
     case RECEIVE_TODO:
       const newTodo = {[action.todo.id]: action.todo};
       return merge({}, state, newTodo);
-    case REMOVE_TODO:
-      nextState = merge({}, state);
-      delete nextState[action.todo.id];
-      return nextState;
     default:
       return state;
   }
