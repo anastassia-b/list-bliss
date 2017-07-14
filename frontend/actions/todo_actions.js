c4814112ac2f6e424ac481b76ab922de3bf3541b
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-13 19:39:52 -0700

    Create base components and actions

---
+++ frontend/actions/todo_actions.js
@@ -0,0 +1,22 @@
+import * as TodoAPIUtil from '../util/todo_api_util';
+
+export const RECEIVE_TODO = "RECEIVE_TODO";
+export const RECEIVE_TODOS = "RECEIVE_TODOS";
+
+export const receiveTodos = todos => ({
+  type: RECEIVE_TODOS,
+  todos
+});
+
+export const receiveTodo = todo => ({
+  type: RECEIVE_TODO,
+  todo
+});
+
+export const fetchTodos = () => dispatch => (
+  TodoAPIUtil.fetchTodos().then(todos => dispatch(receiveTodos(todos)))
+);
+
+export const fetchTodo = () => dispatch => (
+  TodoAPIUtil.fetchTodo().then(todo => dispatch(receiveTodo(todo)))
+);
