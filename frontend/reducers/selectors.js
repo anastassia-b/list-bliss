463acbb83537837acc587d2b5b0a7c48bd158c44
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-07-23 19:52:40 -0700

    Add selector for steps by todo id

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,1 +1,10 @@
 export const allTodos = ({ todos }) => Object.keys(todos).map(id => todos[id]);
+
+export const stepsByTodoId = ({ steps }, todo_id) => {
+  const selectedSteps = [];
+  Object.keys(steps).forEach(stepId => {
+    const step = steps[stepId];
+    if (steps[stepId].todo_id === todo_id) selectedSteps.push(step);
+  });
+  return selectedSteps;
+};
