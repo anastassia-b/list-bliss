045d3e4ecec34b1eaf3ab54c9841844cd81dc827
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-09-14 00:11:09 -0700

    Add user, session dispatch actions

---
+++ frontend/actions/auth_actions.js
@@ -0,0 +1,41 @@
+import * as APIUtil from '../util/auth_api_util'
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_USER = 'RECEIVE_USER';
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveUser = user => ({
+  type: RECEIVE_USER,
+  user
+});
+
+export const fetchUser = id => dispatch => (
+  APIUtil.fetchUser(id)
+    .then(user => dispatch(receiveUser(user)))
+);
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then(user => (
+    dispatch(receiveCurrentUser(null))
+  ))
+);
