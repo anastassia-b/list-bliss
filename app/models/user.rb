f1d60ed724ba18713c6245fd5369213273bc2d8c
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-09-13 23:31:02 -0700

    Add user model, validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: {minimum: 8}
+  validates :username, uniqueness: true
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.valid_password?(password) ? user : nil
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = new_session_token
+    self.save
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= new_session_token
+  end
+
+  def new_session_token
+    SecureRandom.base64
+  end
 end
