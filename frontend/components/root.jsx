c8a3cb596ff95e9d646b17a0df7b3aca81bbf563
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-09-14 00:37:26 -0700

    Add React HashRouter and root route

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import App from './app.jsx';
+import { HashRouter, Route } from 'react-router-dom';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <Route path="/" component={App} />
+    </HashRouter>
   </Provider>
 );
 
