002390dfabb6f3fbd621c3e6f5346de56ee3cd29
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-09-16 22:58:35 -0700

    Rename app, style headings

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,7 +3,8 @@ import TodoListContainer from './todo_list/todo_list_container';
 
 const App = () => (
   <div className="app">
-    <h1>To Do List!</h1>
+    <h1>Welcome to List Bliss!</h1>
+    <h3>Where tasks go when they're finished.</h3>
     <TodoListContainer />
   </div>
 );
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-
 import Root from './components/root';
+
+import { fetchUser, signup, login, logout } from './actions/auth_actions';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  window.store = store;
+  window.dispatch = store.dispatch;
+  window.getState = store.getState;
+  window.fetchUser = fetchUser;
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
