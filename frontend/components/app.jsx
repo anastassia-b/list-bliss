1eb57a94f5f73b9f90b8ed6f03e63fcd2c68dc06
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-09-19 23:32:43 -0700

    Refactor as header component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import TodoListContainer from './todo_list/todo_list_container';
+import Header from './header/header';
 
 const App = () => (
   <div className="app">
+    <Header />
     <h1>Welcome to List Bliss!</h1>
     <h3>Where tasks go when they're finished.</h3>
     <TodoListContainer />
